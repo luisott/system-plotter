e0b8be5f933930820f8dea951bd88756e0ed91c0
Luis Silva Hernandez <luis.silva_hernandez@nokia.com>
2018-04-25 21:40:26 -0400

    Main files and gradle dependencies

---
+++ src/main/java/com/nortey35/SystemPlotter/SystemPlotterApplication.java
@@ -0,0 +1,12 @@
+package com.nortey35.SystemPlotter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemPlotterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemPlotterApplication.class, args);
+	}
+}
---
+++ src/test/java/com/nortey35/SystemPlotter/SystemPlotterApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nortey35.SystemPlotter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SystemPlotterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
